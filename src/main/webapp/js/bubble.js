71508e2ab7b28e95917ea91dd3943b267c99d663
danil emelin <danil228aeedj@mail.ru>
2023-11-17 18:10:22 +0300

    finish welcome page

--- src/main/webapp/js/bubble.js
+++ src/main/webapp/js/bubble.js
@@ -1,3 +1,6 @@
  function changePage() {
      location.replace("http://localhost:8080/Web3-1.0-SNAPSHOT/main.xhtml")
 }
+function changePageBack(){
+     location.replace("http://localhost:8080/Web3-1.0-SNAPSHOT")
+}
